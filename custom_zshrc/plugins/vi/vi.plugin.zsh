44f0b8f57a794e4cafcb347b44cdcb0a9ee17331
Todd Yamakawa <todd.yamakawa@arm.com>
2016-05-26 17:22:04 -0500

    vi.plugin: Adding keymaps.

--- custom_zshrc/plugins/vi/vi.plugin.zsh
+++ custom_zshrc/plugins/vi/vi.plugin.zsh
@@ -2,17 +2,33 @@
 # Select keymap vi
 bindkey -v
 
+
+# =================
+#    INSERT MODE
+# =================
+
 # Ctrl-j and Ctrl-k to search history
-bindkey '^J' down-line-or-search
-bindkey '^K' up-line-or-search
+bindkey -M viins '^J' down-line-or-search
+bindkey -M viins '^K' up-line-or-search
 
-# allow ctrl-p, ctrl-n for navigate history (standard behaviour)
-#bindkey '^P' up-history
-#bindkey '^N' down-history
+# jj or kk to enter command mode
+bindkey -M viins 'jj' vi-cmd-mode
+bindkey -M viins 'kk' vi-cmd-mode
+bindkey -M viins 'jk' vi-cmd-mode
+bindkey -M viins 'kj' vi-cmd-mode
 
-# allow v to edit the command line (standard behaviour)
-#autoload -Uz edit-command-line
-#bindkey -M vicmd 'v' edit-command-line
+# FIXME: Ctrl-backspace to delete word backwards
+#bindkey -M viins '^\b' backward-kill-word
 
+# ==================
+#    COMMAND MODE
+# ==================
 
+# Move to beginning/end of line
+bindkey -M vicmd 'H' vi-beginning-of-line
+bindkey -M vicmd 'L' vi-end-of-line
+
+# Use Vim to edit command line
+autoload -Uz edit-command-line
+bindkey -M vicmd 'v' edit-command-line
 
