3aada07eca5ea03769bb0661643659dff2dd925d
Todd Yamakawa <todd.yamakawa@arm.com>
2016-06-15 11:20:25 -0500

    tmux: Adding aliases and key bindings.

--- custom_zshrc/plugins/tmux/tmux.plugin.zsh
+++ custom_zshrc/plugins/tmux/tmux.plugin.zsh
@@ -3,16 +3,33 @@
 mload gnu/tmux/2.1
 
 # --- Aliases ---
+
+# Force 256 color support
 alias tmux='tmux -2'
-alias tnew='tmux new -s'
-alias topen='tmux attach -t'
-alias tsession='tmux switch-client -t'
+
+# List Sessions
 alias tlist='tmux ls'
-alias tls='tlist'
+alias tls='tmux ls'
+# Attach Session
+alias ta='tmux attach'
+# New session
+alias tnew='tmux new -s'
+# Open session
+alias topen='tmux attach -t'
+# Switch Session
+alias tsession='tmux switch-client -t'
+alias ts='tmux switch-client -t'
+# Last session
+alias tsl='tmux switch-client -l'
+
+# List Keys
 alias tkeys='tmux list-keys'
 alias tkeysvi='tmux list-keys -t vi-copy'
 
+
 # --- Functions ---
+
+# Split Window
 function tsplit() {
 	tmux split-window -d
 	tmux kill-pane -a
