8a781f513a1cdd52a8516b6e11f322b2d19199c0
Todd Yamakawa <todd.yamakawa@arm.com>
2016-07-01 11:55:39 -0500

    zshrc: Adding ARM aliases.

--- custom_zshrc/plugins/arm-secret/arm-secret.plugin.zsh
+++ custom_zshrc/plugins/arm-secret/arm-secret.plugin.zsh
@@ -20,16 +20,33 @@ disass() {
 }
 alias disass_syms='awk '"'"'/^ +\w+$/ {new=$1} /^ +[0-9a-fx]+:/ && new!="" {print $1, new; new=""}'"'"''
 
-# --- SVOS ---
-alias tterminate='touch .TERMINATE'
-alias uart='tailx uart.output'
-alias tarmac_uart="awk '/NS:007ff8/ {num=strtonum(\"0x\"substr(\$6,7,2)); printf(\"%c\", num)}'"
-#alias tarmac_uart_ts="awk '/clk ES/ {clk=\$1} /NS:007ff8/ {num=strtonum(\"0x\"substr(\$6,7,2)); printf(clk\" %c\\n\", num)}'"
-alias tarmac_uart_ts="awk '{print} /NS:007ff8/ {num=strtonum(\"0x\"substr(\$6,7,2)); printf(\"UART %c\\n\", num)}'"
-
 # --- Porter ---
 export PROJ_HOME=/projects/ssg/pj01384_porter/todyam01/porter
 export WORK_DIR=/arm/projectscratch/ssg/pj01384_porter/todyam01/porter
 #source $PROJ_HOME/logical/shared/tools/bin/porter_rtl_setup_bash
 alias new_porter='git clone ssh://hw-gerrit.nahpc.arm.com:29418/systems/porter && cd porter && git checkout dev'
+
+# --- SVOS ---
+alias tterminate='touch .TERMINATE'
+alias uart='tailx uart.output'
+alias tarmac_uart="awk '/S:007ff8/ {num=strtonum(\"0x\"substr(\$6,7,2)); printf(\"%c\", num)}'"
+alias tarmac_uart_ts="awk '{print} /S:007ff8/ {num=strtonum(\"0x\"substr(\$6,7,2)); printf(\"UART %c\\n\", num)}'"
+alias asp_result='echo PASS: RESULT: \"SVOS TEST COMPELTE\" >> RESULT'
+
+# --- Git Repos ---
+alias new_asp='git clone ssh://ds-gerrit.euhpc.arm.com:29418/svos/asp'
+alias new_kvs='git clone login8.nahpc.arm.com:/projects/pd/svos/git/kvs'
+alias new_gem='git clone login8.euhpc.arm.com:/arm/ref/pd/SVOS/tools/gem'
+alias new_ruby='git clone login8.euhpc.arm.com:/arm/ref/pd/SVOS/tools/ruby'
+
+# --- Mail ---
+function mailme {
+	mail -s "\"$@\"" $(whoami)@arm.com
+}
+
+function mailo {
+	mailme "$@" < <($@)
+}
+
+alias mailv='vi /var/spool/mail/$(whoami)'
 
