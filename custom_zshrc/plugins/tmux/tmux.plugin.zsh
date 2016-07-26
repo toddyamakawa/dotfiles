3d935e953329456cfe3726d33033c18a4f50b4f6
Todd Yamakawa <todd.yamakawa@arm.com>
2016-07-26 00:05:13 -0500

    zshrc: Adding tmux aliases.

--- custom_zshrc/plugins/tmux/tmux.plugin.zsh
+++ custom_zshrc/plugins/tmux/tmux.plugin.zsh
@@ -2,30 +2,40 @@
 # --- Module Load ---
 mload gnu/tmux/2.1
 
+
+
 # --- Aliases ---
 
 # Force 256 color support
 alias tmux='tmux -2'
 
-# List Sessions
+# Print Information
+alias tprint='tmux display-message -p'
+alias tlayout="tprint '#{window_layout}'"
+
+# List Information
 alias tls="tmux list-sessions -F '#{session_id} [#{session_windows} windows] #{session_name}'"
 alias tll="tmux list-windows -a -F '#{session_id} #{session_name}:#{window_index} #{window_name} #{pane_current_path}'"
 alias tpanes="tmux list-panes -a -F '#{pane_id} #{session_name}:#{window_index}.#{pane_index} #{window_name} #{pane_current_path}'"
+
+alias tsave="tmux list-panes -a -F '#{session_name} #{window_index} #{window_name} #{pane_current_path} #{window_layout}'"
+
 # Attach Session
 alias ta='tmux attach'
-# New session
+# New Session
 alias tnew='tmux new -s'
-# Open session
+# Open Session
 alias topen='tmux attach -t'
 # Switch Session
 alias tsession='tmux switch-client -t'
 alias ts='tmux switch-client -t'
-# Last session
+# Last Session
 alias tsl='tmux switch-client -l'
 
 # List Keys
 alias tkeys='tmux list-keys'
 alias tkeysvi='tmux list-keys -t vi-copy'
+
 
 # --- Functions ---
 
