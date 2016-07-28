58a7b9e1af4e9face87ec7a2fbf8aa82987c7942
Todd Yamakawa <todd.yamakawa@arm.com>
2016-07-28 17:56:59 -0500

    zshrc: Updates tmux display and $DISPLAY.

--- custom_zshrc/plugins/magic-enter/magic-enter.plugin.zsh
+++ custom_zshrc/plugins/magic-enter/magic-enter.plugin.zsh
@@ -2,9 +2,19 @@
 # --- Widget ---
 zle -N magic-enter
 
-# --- Function ---
+# --- Functions ---
+
+# Runs everytime enter is pressed
 function magic-enter() {
+	set-title-buffer
+	[[ -f ~/.DISPLAY ]] && export DISPLAY=$(cat ~/.DISPLAY)
+	[[ -n $TMUX ]] && tmux set-environment DISPLAY $DISPLAY
 	[[ -z $BUFFER ]] && zle clear-screen || zle accept-line
+}
+
+# Set title to $BUFFER
+function set-title-buffer() {
+	print -Pn "\e]0;$BUFFER\a"
 }
 
 # --- Key Binding ---
