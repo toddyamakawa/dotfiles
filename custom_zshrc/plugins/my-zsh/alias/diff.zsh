c1ace8a8b43aba4d25ecc400952b81d800f8e6f9
Todd Yamakawa <todd.yamakawa@arm.com>
2016-08-05 10:52:16 -0500

    zshrc: Updating meld and colordiff modules.

--- custom_zshrc/plugins/my-zsh/alias/diff.zsh
+++ custom_zshrc/plugins/my-zsh/alias/diff.zsh
@@ -1,15 +1,16 @@
 
 # --- meld ---
-MODULE_MELD=meld/meld/1.5.3
+#MODULE_MELD=meld/meld/1.5.3
 #MODULE_MELD=meld/meld/1.8.2
 #MODULE_MELD=meld/meld/1.8.4
 #MODULE_MELD=meld/meld/3.12.3
+MODULE_MELD=meld/meld/3.14.0
 
 # --- tkdiff ---
 MODULE_TKDIFF=tkdiff/tkdiff/4.2
 
 # --- colordiff ---
-MODULE_COLORDIFF=colordiff
+MODULE_COLORDIFF=colordiff/colordiff/1.0.16
 alias diff='colordiff'
 alias colordiff='colordiff -w'
 
