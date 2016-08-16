ae91d5b9b3efbf4ffa7a0a4357f0f2b6fa569d0d
Todd Yamakawa <todd.yamakawa@arm.com>
2016-08-16 15:42:02 -0500

    zshrc: Adding ps aliases.

--- custom_zshrc/plugins/my-zsh/alias/ps.zsh
+++ custom_zshrc/plugins/my-zsh/alias/ps.zsh
@@ -1,5 +1,7 @@
 
 # --- ps ---
+alias -g pso='-o %cpu,%mem,user,pid,ppid,etime,time,cmd'
+alias psort='ps -e pso | grep -v grep | sort -n'
 alias psef='ps -ef | grep -v grep'
 alias psaux='ps auxw | grep -v grep | sort -n -k3'
 alias psefg='psef | grep'
