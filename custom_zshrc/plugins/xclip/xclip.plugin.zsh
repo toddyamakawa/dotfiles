199f7440676e0a0276d8d23460506585a616a4a8
Todd Yamakawa <todd.yamakawa@arm.com>
2016-08-26 11:08:13 -0500

    zshrc: Adding xclip aliases for LSF.

--- custom_zshrc/plugins/xclip/xclip.plugin.zsh
+++ custom_zshrc/plugins/xclip/xclip.plugin.zsh
@@ -4,19 +4,25 @@ mload xclip/xclip/0.12
 
 # --- Copy/Paste ---
 function copy() { eval "$@" | xclip; }
-alias paste='xclip -o'
+alias -g paste='$(xclip -o)'
+alias echop='echo Clipboard: paste'
 
-# --- xclip ---
-alias echop='echo "Clipboard: $(paste)"'
-alias pwdc='copy pwd && echop'
-alias cdp='echo_eval cd $(paste)'
-alias cpp.='echo_eval cp $(paste) .'
-alias lnsp='echo_eval ln -s $(paste)'
-
+# --- Files/Directories ---
 function clip() { copy readlink -f $1; echop; }
+alias pwdc='copy pwd && echop'
+alias cdp='echo_eval cd paste'
+alias cpp.='echo_eval cp paste .'
+alias lnsp='echo_eval ln -s paste'
 
-alias meldp='echo_eval meld $(paste)'
-alias svncop='echo_eval svn co $(paste)'
+# --- LSF ---
+alias bjobsp='echo_eval bjobs paste'
+alias bjobsup='echo_eval bjobsu paste'
+function bmodp() { echo_eval bmod "$@" paste; }
 
+# --- SVN ---
+alias svncop='echo_eval svn co paste'
 alias svnlink='svn info | awk '"'"'/^URL/ {print $2}'"'"' | xclip && echop'
+
+# --- Meld ---
+alias meldp='echo_eval meld $(paste)'
 
