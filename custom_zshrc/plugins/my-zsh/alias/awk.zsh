2bbc7e18b5d70c7e33b00fea196d4565b3799594
Todd Yamakawa <todd.yamakawa@arm.com>
2016-09-09 11:31:12 -0500

    zshrc: Adding awk aliases.

--- custom_zshrc/plugins/my-zsh/alias/awk.zsh
+++ custom_zshrc/plugins/my-zsh/alias/awk.zsh
@@ -1,7 +1,11 @@
 
 # --- awk columns ---
 function awkc() { awk '{print $'$1'}'; }
-for i in $(seq 1 11); do alias awk$i="awkc $i"; done
+for i in $(seq 1 11); do alias -g col$i="| awkc $i"; done
+
+# --- awk rows ---
+function awkr() { awk '(NR=='$1'){print $0}'; }
+for i in $(seq 1 11); do alias -g row$i="| awkr $i"; done
 
 function regex() {
 	awk 'match($0,/'$1'/, groups) {print groups['${2:-'0'}']}'
