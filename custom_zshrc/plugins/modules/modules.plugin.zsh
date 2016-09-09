77e0aa1d7d45a5ed55c1681362e618cf53f85f0b
Todd Yamakawa <todd.yamakawa@arm.com>
2016-09-09 11:21:21 -0500

    zshrc: Adding Python module.

--- custom_zshrc/plugins/modules/modules.plugin.zsh
+++ custom_zshrc/plugins/modules/modules.plugin.zsh
@@ -17,6 +17,7 @@ mload git/git/2.7.0 vim/vim/7.4
 
 # --- Scripting languages ---
 alias rubyload='mload ruby/ruby/2.2.1'
+alias pythonload='mload python/python/3.5.1'
 alias perlload='mload perl/perl/5.12.3'
 alias tclload='mload tct/tcl/8.6.1'
 alias javaload='mload sun/jdk/1.8.0_77'
