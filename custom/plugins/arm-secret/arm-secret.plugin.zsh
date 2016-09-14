893eff9ac6b045b5fc9b39ce55c0618958ad3e01
Todd Yamakawa <todd.yamakawa@arm.com>
2016-09-14 15:15:29 -0500

    zshrc: Fixing LSB_DEFAULTPROJECT setting.

--- custom/plugins/arm-secret/arm-secret.plugin.zsh
+++ custom/plugins/arm-secret/arm-secret.plugin.zsh
@@ -9,8 +9,8 @@ alias euhpc='sitename | \grep -q euhpc'
 alias getquota=/usr/local/bin/getquota
 
 # --- Environment Variables ---
-euhpc && export LSB_DEFAULTPROJECT=PJ01384
-nahpc && export LSB_DEFAULTPROJECT=PJ01384DEFAULT
+euhpc && [[ -z $LSB_BATCH_JID ]] && export LSB_DEFAULTPROJECT=PJ01384
+nahpc && [[ -z $LSB_BATCH_JID ]] && export LSB_DEFAULTPROJECT=PJ01384DEFAULT
 
 # --- Disassemble ---
 function disass() {
