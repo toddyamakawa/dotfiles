b8a5ff60a1cf2f0f204c7198cba5ab245d6e598a
Todd Yamakawa <todd.yamakawa@arm.com>
2016-09-26 11:26:44 -0500

    zshrc: Adding asp_run2log function.

--- custom/plugins/arm-secret/arm-secret.plugin.zsh
+++ custom/plugins/arm-secret/arm-secret.plugin.zsh
@@ -32,6 +32,16 @@ alias uart='tailx uart.output'
 alias tarmac_uart="awk '/S:007ff80000/ {num=strtonum(\"0x\"substr(\$6,7,2)); printf(\"%c\", num)}'"
 alias tarmac_uart_ts="awk '{print} /S:007ff80000/ {num=strtonum(\"0x\"substr(\$6,7,2)); printf(\"UART %c\\n\", num)}'"
 alias asp_result='echo PASS: RESULT: \"SVOS TEST COMPELTE\" >> RESULT'
+function asp_run2log() {
+	local timestamp=$(date +%y_%m_%d_%H_%M_%S -r config.json)
+	local testpath=${PWD##*run_dir/}
+	local testname=${testpath%/*}
+	local runtype=${testpath#*/}
+	local newdir=$(readlink -f $PWD/../../../log_dir/$testname-$runtype-$timestamp)
+	mv $PWD $newdir
+	cd $newdir
+}
+
 function tarmac_arg() {
 	tclsh <(echo 'source arg.tcl; puts "-tracestart $tarmac(start) -traceend $tarmac(end)"')
 }
