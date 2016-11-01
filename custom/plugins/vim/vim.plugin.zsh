37bcdf107e396de48c421d8deed687c42e095b8c
Todd Yamakawa <todd.yamakawa@arm.com>
2016-11-01 09:06:20 -0500

    zshrc: Updating vim plugin.

--- custom/plugins/vim/vim.plugin.zsh
+++ custom/plugins/vim/vim.plugin.zsh
@@ -3,6 +3,7 @@
 export EDITOR=vim
 export MANPAGER="vim -c MANPAGER -"
 
+
 # --- Aliases ---
 alias vi="vim"
 alias vless="col -b | vim -c 'set ts=8 nomod nolist' -"
@@ -10,4 +11,18 @@ alias vman='vim -c MANPAGER -'
 alias vmin='vim --noplugin'
 alias vnone='vim -u NONE'
 alias vdiff='vimdiff'
+
+
+# --- Functions ---
+
+# Find tmux session with vim .swp file
+function vtfind() {
+	#pid=$(lsof -t $1 2>/dev/null)
+	local pid=$(lsof -t $1)
+	echo "PID: $pid"
+	local pts=$(ps -o tty= -p $pid)
+	echo "PTS: $pts"
+	session=$(tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index} #{pane_tty}' | grep $pts)
+	echo $session
+}
 
