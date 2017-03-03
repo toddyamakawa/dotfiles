974a81a0e70c7f745b543d505e6dc05d8409df18
Todd Yamakawa <todd.yamakawa@arm.com>
2017-03-03 13:31:22 -0600

    zshrc/vi-mode: Updating chorded key-bindings.

--- custom/plugins/vi-mode/vi-mode.plugin.zsh
+++ custom/plugins/vi-mode/vi-mode.plugin.zsh
@@ -1,8 +1,13 @@
 
-# ===============
-#    VI KEYMAP
-# ===============
+# ========================
+#    VI KEYMAP SETTINGS
+# ========================
+
+# Select vi keymap
 bindkey -v
+
+# Turn two-key key-bindings into a chorded key-bindings
+export KEYTIMEOUT=2
 
 
 # ==================
@@ -14,6 +19,27 @@ function zkeys() {
 	done
 }
 
+
+# =============
+#    WIDGETS
+# =============
+
+zle -N vi-up-begin
+function vi-up-begin() {
+	zle up-line-or-history
+	zle vi-beginning-of-line
+}
+
+zle -N vi-up-end
+function vi-up-end() {
+	zle up-line-or-history
+	zle vi-add-eol
+}
+
+zle -N vi-xclip-paste
+function vi-xclip-paste() {
+	xclip -o | xargs echo -n
+}
 
 # =================
 #    INSERT MODE
@@ -29,10 +55,21 @@ bindkey -M viins '^K' history-search-backward
 #bindkey -M viins '^J' history-incremental-search-forward
 #bindkey -M viins '^K' history-incremental-search-backward
 
-# Hit 'j' and 'k' simultaneously to enter vicmd mode
-export KEYTIMEOUT=2
+# [j+k] to enter vicmd mode
 bindkey -M viins 'jk' vi-cmd-mode
 bindkey -M viins 'kj' vi-cmd-mode
+
+# [k+h] to go up and to the beginning of the line
+bindkey -M viins 'kh' vi-up-begin
+bindkey -M viins 'hk' vi-up-begin
+
+# [k+l] to go up and to the end of the line
+bindkey -M viins 'kl' vi-up-end
+bindkey -M viins 'lk' vi-up-end
+
+# [o+p] to paste
+bindkey -M viins 'op' vi-xclip-paste
+bindkey -M viins 'po' vi-xclip-paste
 
 # FIXME: Ctrl-backspace to delete word backwards, doesn't work in gnome-terminal
 #bindkey -M viins '^?' backward-kill-word
