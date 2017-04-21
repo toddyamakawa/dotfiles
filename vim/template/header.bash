df74406c0b5658f13a66dc50e21a2e9ab31fc2ae
Todd Yamakawa <todd.yamakawa@arm.com>
2017-04-21 17:00:08 -0500

    vimrc: Updating bash template.

--- vim/template/header.bash
+++ vim/template/header.bash
@@ -1,3 +1,22 @@
 #!/bin/bash
 script=$(readlink -f $0)
 here=$(dirname $script)
+
+declare -A ARGV
+declare -a ARGS
+while [[ $# -gt 1 ]]; do
+	arg="$1" && shift
+	case "$arg" in
+		-*) ARGV["$arg"]="$1"; shift;;
+		*) ARGS=("${ARGS[@]}" "$arg");;
+	esac
+done
+
+for arg in "${!ARGV[@]}"; do
+	echo "$arg ${ARGV[$arg]}"
+done
+
+for arg in "${ARGS[@]}"; do
+	echo "- $arg"
+done
+
