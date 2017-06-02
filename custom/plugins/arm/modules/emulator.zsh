de5a7851a581e58a8ccc558a3d5d866ca477bb4a
Todd Yamakawa <todd.yamakawa@arm.com>
2017-06-02 13:18:37 -0500

    zshrc/arm: Updating emulator-related aliases.

--- custom/plugins/arm/modules/emulator.zsh
+++ custom/plugins/arm/modules/emulator.zsh
@@ -1,13 +1,16 @@
 
-# --- fasttrack ---
-function fasttrack() {
-	/arm/warehouse/TOEF/scheduler/0.1/83/linux-x86_64/bin/fasttrack $@
-}
-
 # --- Global Aliases ---
 alias -g -- '-velconf'='-conf /home/lsf/scheduler_prod/configs/NAHPCVEL/NAHPCVEL.conf'
 alias -g -- '-zs3conf'='-conf /home/lsf/scheduler_prod/configs/NAHPCPRODZS3/NAHPCPRODZS3.conf'
 
 # --- Veloce ---
 alias velres='cat /arm/cluster/nahpc/conf/veloce-scheduler.cfg'
+
+# --- Emululation Tools ---
+alias emu='mrun +arm/emutools'
+alias emuhelp='ls -l $(dirname $(emu which emustat))'
+alias velstat='emutools velstat'
+alias zs3stat='emutools zs3stat'
+alias velft='emutools velfasttrack'
+alias zs3ft='emutools zs3fasttrack'
 
