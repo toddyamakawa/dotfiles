9f51a1dd9996e0d52fd395c22751088ec34b2613
Todd Yamakawa <todd.yamakawa@arm.com>
2017-06-05 10:35:53 -0500

    zshrc/vim: Adding swp alias.

--- custom/plugins/vim/vim.plugin.zsh
+++ custom/plugins/vim/vim.plugin.zsh
@@ -5,15 +5,26 @@ export MANPAGER="vim -c MANPAGER -"
 
 
 # --- Aliases ---
-alias vi="vim"
+alias vi=vim
 alias vless="col -b | vim -c 'set ts=8 nomod nolist' -"
 alias vman='vim -c MANPAGER -'
 alias vmin='vim --noplugin'
 alias vnone='vim -u NONE'
 alias vdiff='vimdiff'
+alias vswp='vim -r 2>&1'
 
 
 # --- Functions ---
+
+function vimls() {
+	vswp | awk '/^[0-9]/{file=$2}/process ID:/{print $0, file}'
+	#return
+	#for file in $(vswp | awk '/^[0-9]/{file=$2}/process ID:/{print $3, file}'); do
+		#pid=$(lsof -t $(echo $file)) || continue
+		#echo a: $pid $file
+	#done
+}
+#vimls
 
 # Find tmux session with vim .swp file
 function vtfind() {
