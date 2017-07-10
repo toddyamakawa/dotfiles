ce4d3f3db615e7cae2334aa19ea579a0aa41f3b5
Todd Yamakawa <todd.yamakawa@arm.com>
2017-07-10 16:13:58 -0500

    zshrc/arm: Updating modules.

--- custom/plugins/arm/modules/disass.zsh
+++ custom/plugins/arm/modules/disass.zsh
@@ -1,8 +1,8 @@
 
 # --- Disassemble ---
+alias fromelf='mrun +arm/rvct_aarch64 fromelf'
 function disass() {
 	local file=$1
-	module load arm/rvct_aarch64
 	bs1 -oo $file.disass fromelf -a -c --text $file
 }
 alias disass_syms='awk '"'"'/^ +\w+$/ {new=$1} /^ +[0-9a-fx]+:/ && new!="" {print $1, new; new=""}'"'"''
--- custom/plugins/arm/modules/pdf.zsh
+++ custom/plugins/arm/modules/pdf.zsh
@@ -1,6 +1,3 @@
 
-function pdf() {
-	module load adobe/acrobat/8.1.3
-	command acroread $@
-}
+alias pdf='mrun +adobe/acrobat/8.1.3 acroread'
 
