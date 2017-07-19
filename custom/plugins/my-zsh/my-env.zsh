6e232710b050409275a5ec631d0a2511b32fe173
Todd Yamakawa <todd.yamakawa@arm.com>
2017-07-19 16:24:54 -0500

    zshrc/my-env: Disabling XON/XOFF flow control.

--- custom/plugins/my-zsh/my-env.zsh
+++ custom/plugins/my-zsh/my-env.zsh
@@ -4,7 +4,14 @@ export TERM=xterm-256color
 export PAGER='less -r'
 unset LESS
 
-# Print colors
+
+# --- Terminal Line Settings ---
+
+# Disable XON/XOFF flow control
+stty -ixon
+
+
+# --- Colors ---
 function zsh_colors() {
 	for color in RED YELLOW GREEN BLUE CYAN MAGENTA BLACK WHITE; do
 		echo "$fg_bold[${(L)color}]BOLD $fg_no_bold[${(L)color}]$color"
