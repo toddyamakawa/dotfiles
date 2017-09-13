9dea1aeda4457f799b2c5d66cf405473754c206d
Todd Yamakawa <todd.yamakawa@arm.com>
2017-09-13 11:58:28 -0500

    zshrc: Updating diff aliases.

--- custom/plugins/my-zsh/alias/diff.zsh
+++ custom/plugins/my-zsh/alias/diff.zsh
@@ -1,19 +1,11 @@
 
 # --- meld ---
-#MODULE_MELD=meld/meld/1.5.3
-#MODULE_MELD=meld/meld/1.8.2
-#MODULE_MELD=meld/meld/1.8.4
-#MODULE_MELD=meld/meld/3.12.3
-MODULE_MELD=meld/meld/3.14.0
+meld() { mrun +meld/meld meld $@; }
 
 # --- tkdiff ---
-MODULE_TKDIFF=tkdiff/tkdiff/4.2
+tkdiff() { mrun +tkdiff/tkdiff tkdiff -w $@; }
 
 # --- colordiff ---
-MODULE_COLORDIFF=colordiff/colordiff/1.0.16
 alias diff='colordiff'
-alias colordiff='colordiff -w'
-
-# --- Load Modules ---
-mload $MODULE_MELD $MODULE_TKDIFF $MODULE_COLORDIFF
+colordiff() { mrun +colordiff/colordiff colordiff -w $@; }
 
