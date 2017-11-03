5a31964382de69ff1b726b184d7b0b991a04bfc6
Todd Yamakawa <todd.yamakawa@arm.com>
2017-11-03 09:21:05 -0500

    zshrc/vim: Added TODO items.

--- custom/plugins/vim/vim.plugin.zsh
+++ custom/plugins/vim/vim.plugin.zsh
@@ -11,29 +11,30 @@ alias vman='vim -c MANPAGER -'
 alias vmin='vim --noplugin'
 alias vnone='vim -u NONE'
 alias vdiff='vimdiff'
-alias vswp='vim -r 2>&1'
 
 
 # --- Functions ---
 
-function vimls() {
-	vswp | awk '/^[0-9]/{file=$2}/process ID:/{print $0, file}'
-	#return
-	#for file in $(vswp | awk '/^[0-9]/{file=$2}/process ID:/{print $3, file}'); do
-		#pid=$(lsof -t $(echo $file)) || continue
-		#echo a: $pid $file
-	#done
+# TODO: Clean up this function
+function vimswp() {
+	vim -r 2>&1 | awk '/^[0-9]/{file=$2}/process ID:/{print $0, file}'
 }
-#vimls
 
+# TODO: Make this function work
 # Find tmux session with vim .swp file
 function vtfind() {
+	local -U pids
+	vimswp | command grep -Po 'process ID: \K[0-9]+'
+	#pids=(vimswp | command grep -Po 'process ID: \K[0-9]+')
+	#for pid in $pids; do
+		#echo "a $pid"
+	#done
 	#pid=$(lsof -t $1 2>/dev/null)
-	local pid=$(lsof -t $1)
-	echo "PID: $pid"
-	local pts=$(ps -o tty= -p $pid)
-	echo "PTS: $pts"
-	session=$(tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index} #{pane_tty}' | grep $pts)
-	echo $session
+	#local pid=$(lsof -t $1)
+	#echo "PID: $pid"
+	#local pts=$(ps -o tty= -p $pid)
+	#echo "PTS: $pts"
+	#session=$(tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index} #{pane_tty}' | grep $pts)
+	#echo $session
 }
 
