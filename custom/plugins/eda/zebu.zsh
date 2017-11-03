f36ec2dc9bd12cd1c19a9ca04c791d199acf354c
Todd Yamakawa <todd.yamakawa@arm.com>
2017-11-03 09:12:34 -0500

    zshrc/zebu: Adding functions.

--- custom/plugins/eda/zebu.zsh
+++ custom/plugins/eda/zebu.zsh
@@ -6,3 +6,18 @@ alias zebuload='mload synopsys/zebu/2015.09-7_engfix_EF1'
 alias zjobs='zSpy -batch -systemDir /arm/local/synopsys/zebu/zebu_system_dir_ZS3'
 alias zfree='zSpy -free -systemDir /arm/local/synopsys/zebu/zebu_system_dir_ZS3'
 
+# TODO: Figure out how to get this to work
+#function zebu_pwd() {
+	#local temp=${PWD##*/zebu*
+#}
+
+# TODO: Use zebu_pwd
+function zebu_size() {
+	ls -ldh build/zcui.work/backend_default/U?/M?
+}
+
+# TODO: Use zebu_pwd
+function zebu_sum_cut() {
+	grep 'SUM CUT' build/zcui.work/backend_default/Work.Part_*/zCoreBuild.log
+}
+
