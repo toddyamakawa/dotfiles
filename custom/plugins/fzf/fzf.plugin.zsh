9fcc4853da8b92d4a5d5aad61691f4690086569c
Todd Yamakawa <todd.yamakawa@arm.com>
2017-11-15 19:01:24 -0600

    zshrc/fzf: Added widget with S-Tab key-binding.

--- custom/plugins/fzf/fzf.plugin.zsh
+++ custom/plugins/fzf/fzf.plugin.zsh
@@ -10,8 +10,28 @@ source ~/.fzf.zsh
 bindkey -M vicmd '^R' fzf-history-widget
 bindkey -M viins '^R' fzf-history-widget
 
-bindkey -M vicmd '^T' fzf-file-widget
-bindkey -M viins '^T' fzf-file-widget
+bindkey -M viins '^[[Z' fzf-magic-complete
+bindkey -M viins '^[[Z' fzf-magic-complete
+
+
+# --- Widget ---
+# Create fzf-magic-complete widget
+zle -N fzf-magic-complete
+function fzf-magic-complete() {
+	if [[ $LBUFFER =~ '^cd\b' ]]; then
+		local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+			-o -type d -print 2> /dev/null | cut -b3-"}"
+		setopt localoptions pipefail 2> /dev/null
+		local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
+		LBUFFER="${LBUFFER}$dir"
+	else
+		LBUFFER="${LBUFFER}$(__fsel)"
+	fi
+	local ret=$?
+	zle redisplay
+	typeset -f zle-line-init >/dev/null && zle zle-line-init
+	return $ret
+}
 
 
 # --- Aliases ---
