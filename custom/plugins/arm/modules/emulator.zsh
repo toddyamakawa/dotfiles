2ff9a760bfcc127caa217f934602c68fed9a9ea4
Todd Yamakawa <todd.yamakawa@arm.com>
2018-01-17 11:55:15 -0600

    zshrc/emulator: Renaming emu alias to emutools.

--- custom/plugins/arm/modules/emulator.zsh
+++ custom/plugins/arm/modules/emulator.zsh
@@ -7,14 +7,14 @@ alias -g -- '-zs3conf'='-conf /home/lsf/scheduler_prod/configs/NAHPCPRODZS3/NAHP
 alias velres='cat /arm/cluster/nahpc/conf/veloce-scheduler.cfg'
 
 # --- Emululation Tools ---
-alias emu='mrun +arm/emutools'
-alias emuhelp='ls -l $(dirname $(emu which emustat))'
-alias veljobs='emu veljobs -user all'
-alias zs3jobs='emu zs3jobs -user all'
-alias velstat='emu velstat'
-alias zs3stat='emu zs3stat'
-alias velft='emu velfasttrack'
-alias zs3ft='emu zs3fasttrack'
+alias emutools='mrun +arm/emutools'
+alias emuhelp='ls -l $(dirname $(emutools which emustat))'
+alias veljobs='emutools veljobs -user all'
+alias zs3jobs='emutools zs3jobs -user all'
+alias velstat='emutools velstat'
+alias zs3stat='emutools zs3stat'
+alias velft='emutools velfasttrack'
+alias zs3ft='emutools zs3fasttrack'
 
 # Find defunct processes on emulation hosts
 function emu_zombie() {
