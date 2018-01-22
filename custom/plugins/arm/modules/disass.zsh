5b90ff6a741a13c67c93eeb9fc2b07be8e00724e
Todd Yamakawa <todd.yamakawa@arm.com>
2018-01-22 10:04:30 -0600

    zshrc/arm: Updating disass function.

--- custom/plugins/arm/modules/disass.zsh
+++ custom/plugins/arm/modules/disass.zsh
@@ -2,7 +2,8 @@
 # --- Disassemble ---
 function disass() {
 	local file=$1
-	bs1 -oo $file.disass mrun +arm/rvct_aarch64 fromelf -a -c --text $file
+	bs1 -rn -app FG -K -oo $file.disass \
+		mrun +arm/rvct_aarch64 fromelf -a -c --text $file
 }
 alias disass_syms='awk '"'"'/^ +\w+$/ {new=$1} /^ +[0-9a-fx]+:/ && new!="" {print $1, new; new=""}'"'"''
 
