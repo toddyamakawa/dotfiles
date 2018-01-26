d8b1626a6451f810f52a9c420821768508619d46
Todd Yamakawa <todd.yamakawa@arm.com>
2018-01-26 09:51:20 -0600

    zshrc/themes: Updating elapsed_ms handling preexec/precmd.

--- custom/themes/pre.zsh
+++ custom/themes/pre.zsh
@@ -7,6 +7,7 @@
 function preexec() {
 	_set-display $(_find-display)
 	_set-powerline
+	unset elapsed_ms
 	start_ms=$(_current-ms)
 }
 
@@ -18,7 +19,7 @@ function preexec() {
 # --- precmd() ---
 # Record elapsed time
 function precmd() {
-	elapsed_ms=$(($(date +%s%3N)-$start_ms))
+	elapsed_ms=${elapsed_ms-$(($(date +%s%3N)-$start_ms))}
 	[[ -e ${TMUX%%,*} ]] && tmux refresh-client
 	# TODO: Fix MAGIC_ENTER_BUFFER
 	#_autonotify
