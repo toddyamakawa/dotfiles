b5c3afee47e154cfeb3b71228b509ae7f40f0bda
Todd Yamakawa <todd.yamakawa@arm.com>
2018-04-05 15:18:03 -0500

    zshrc/eda/questa: Updating UCDB functions.

--- custom/plugins/eda/questa.zsh
+++ custom/plugins/eda/questa.zsh
@@ -2,11 +2,21 @@
 # --- Modules ---
 alias questaload='mload mentor/questasim/10.7_1'
 
+
 # --- Aliases ---
 alias ucdb_report_parse="awk '/^\s*CLASS/{class=\$2} /^\s*TYPE/{type=\$2} /^\s*bin/{print class,type,\$2,\$3,\$4,\$5}'"
 
+
 # --- Functions ---
+
+# Dump and parse UCDB file into one-liners
 function ucdb_dump() {
+	which vcover || return 1
 	vcover report -details $@ | ucdb_report_parse
 }
 
+# View UCDB in GUI
+function ucdb_view() {
+	which vsim || return 1
+	bs8 -app FG -Is -XF vsim -gui -viewcov $@
+}
