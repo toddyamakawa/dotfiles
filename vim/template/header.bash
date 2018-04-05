38f288efeae1f3d777a4f69ae2dbdba8f786fee0
Todd Yamakawa <todd.yamakawa@arm.com>
2018-04-05 12:28:37 -0500

    vimrc/template: Fixing shift bug in header.bash.

--- vim/template/header.bash
+++ vim/template/header.bash
@@ -7,7 +7,7 @@ declare -a ARGS
 while [[ $# -ge 1 ]]; do
 	arg="$1" && shift
 	case "$arg" in
-		-*) ARGV["$arg"]="$1"; shift;;
+		-*) [[ $1 == ${1#-} ]] && { ARGV["$arg"]="$1"; shift; } || ARGV["$arg"]=1;;
 		*) ARGS=("${ARGS[@]}" "$arg");;
 	esac
 done
