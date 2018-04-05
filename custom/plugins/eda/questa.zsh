fb08ea9db1e8b744c212bceefde11bdc18a56499
Todd Yamakawa <todd.yamakawa@arm.com>
2018-04-05 12:08:10 -0500

    zshrc/eda/questa: Adding UCDB function and alias.

--- custom/plugins/eda/questa.zsh
+++ custom/plugins/eda/questa.zsh
@@ -2,3 +2,11 @@
 # --- Modules ---
 alias questaload='mload mentor/questasim/10.7_1'
 
+# --- Aliases ---
+alias ucdb_report_parse="awk '/^\s*CLASS/{class=\$2} /^\s*TYPE/{type=\$2} /^\s*bin/{print class,type,\$2,\$3,\$4,\$5}'"
+
+# --- Functions ---
+function ucdb_dump() {
+	vcover report -details $@ | ucdb_report_parse
+}
+
