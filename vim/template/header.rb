fd372cd8b365d961e7285eefbfe83b2a178323a8
Todd Yamakawa <todd.yamakawa@arm.com>
2018-04-06 12:30:39 -0500

    vimrc/template: Updating Ruby template.

--- vim/template/header.rb
+++ vim/template/header.rb
@@ -1,6 +1,30 @@
 #!/usr/bin/env ruby
+
+require 'optparse'
+
 $here = File.dirname(__FILE__)
 
 if __FILE__ == $0
+	options = Hash.new
+	parser = OptionParser.new "Usage: #{$0} [options] [FILE]" do |opts|
+		opts.on '-h', '--help', 'Show this message' do
+			puts opts
+			exit
+		end
+		opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+			options[:verbose] = v
+		end
+	end
+
+	begin
+		parser.parse!
+	rescue OptionParser::InvalidOption => e
+		puts e
+		puts parser
+		exit
+	end
+
+	p options
+	puts "ARGV: #{ARGV.join(' ')}"
 end
 
