84b3d4ba8f921d8bedd9a21d5853e396de9d36d5
Todd Yamakawa <todd.yamakawa@arm.com>
2018-04-19 17:05:27 -0500

    vim/template/header.bash: Adding function to trap on EXIT.

--- vim/template/header.bash
+++ vim/template/header.bash
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 script=$(readlink -f $BASH_SOURCE)
 here=$(dirname $script)
+top=$(git -C $here rev-parse --show-toplevel 2>/dev/null)
 
+# Immediately exit on failure
+set -e
+
+# Always run when script finishes
+function finish() {
+	echo Cleaning up...
+}
+trap finish EXIT
+
+# Parse arguments
 declare -A ARGV
 declare -a ARGS
 while [[ $# -ge 1 ]]; do
@@ -12,14 +23,17 @@ while [[ $# -ge 1 ]]; do
 	esac
 done
 
+# Print argments with value
 for arg in "${!ARGV[@]}"; do
 	echo "$arg ${ARGV[$arg]}"
 done
 
+# Print flags
 for arg in "${ARGS[@]}"; do
 	echo "- $arg"
 done
 
+# Source setup scripts if available
 if [[ -d /arm/tools/ ]]; then
 	source /arm/tools/setup/init/bash
 	module load core eda swdev util arm/cluster/2.0
