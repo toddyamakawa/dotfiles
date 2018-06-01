7f4bf5f3c22fa864973daa2ee4c1d6ea7d26eedd
Todd Yamakawa <todd.yamakawa@arm.com>
2018-06-01 11:22:14 -0500

    zshrc/arm: Updating CPG aliases.

--- zsh/custom/plugins/arm/modules/cpg-val.zsh
+++ zsh/custom/plugins/arm/modules/cpg-val.zsh
@@ -1,10 +1,20 @@
 
-# --- Module ---
+# --- Aliases ---
+alias detag=/arm/devsys-tools/abs/detag
+
+# Zeus module
 alias -g zeusmodule='$(git rev-parse --show-toplevel)/tools/modules/zeus_trial'
 alias zeusload='module load zeusmodule'
 
+
 # --- disasm ---
-export EBM_VERSION=$(/arm/devsys-tools/abs/detag PerformanceModeling:EBM:master::trunk)
+
+# Rosetta
+export ROSETTA_VERSION=$(detag ARMCC:Rosetta:0.0)
+alias rose='/arm/warehouse/ARMCC/Rosetta/0.0/$ROSETTA_VERSION/dynamic/linux-x86_64-rel-public/gcc-4.4.4/bin/rosetta-api-test -d'
+
+# EBM
+export EBM_VERSION=$(detag PerformanceModeling:EBM:master::trunk)
 alias ares_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$EBM_VERSION/Linux/x86_64/gcc-4.9.2/rel_int/bin/ares_disasm'
 alias deimos_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$EBM_VERSION/Linux/x86_64/gcc-4.9.2/rel_int/bin/deimos_disasm'
 alias zeus_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$EBM_VERSION/Linux/x86_64/gcc-4.9.2/rel_int/bin/zeus_disasm'
