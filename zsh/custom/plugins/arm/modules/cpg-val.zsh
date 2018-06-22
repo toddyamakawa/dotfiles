fc86388e9907b42cceacc0f483e4551b02d85430
Todd Yamakawa <todd.yamakawa@arm.com>
2018-06-22 12:26:17 -0500

    zshrc/cpg-val: Moving environment variables into functions for performance reasons.

--- zsh/custom/plugins/arm/modules/cpg-val.zsh
+++ zsh/custom/plugins/arm/modules/cpg-val.zsh
@@ -6,18 +6,22 @@ alias detag=/arm/devsys-tools/abs/detag
 alias -g zeusmodule='$(git rev-parse --show-toplevel)/tools/modules/zeus_trial'
 alias zeusload='module load zeusmodule'
 
-
-# --- disasm ---
-
 # Rosetta
-export ROSETTA_VERSION=$(detag ARMCC:Rosetta:0.0)
-alias rose='/arm/warehouse/ARMCC/Rosetta/0.0/$ROSETTA_VERSION/dynamic/linux-x86_64-rel-public/gcc-4.4.4/bin/rosetta-api-test -d'
+function _detag_rosetta() {
+	export ROSETTA_VERSION=${ROSETTA_VERSION-$(detag ARMCC:Rosetta:0.0)}
+	echo $ROSETTA_VERSION
+}
+
+alias rose='/arm/warehouse/ARMCC/Rosetta/0.0/$(_detag_rosetta)/dynamic/linux-x86_64-rel-public/gcc-4.4.4/bin/rosetta-api-test -d'
 
 # EBM
-export EBM_VERSION=$(detag PerformanceModeling:EBM:master::trunk)
-alias ares_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$EBM_VERSION/Linux/x86_64/gcc-4.9.2/rel_int/bin/ares_disasm'
-alias deimos_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$EBM_VERSION/Linux/x86_64/gcc-4.9.2/rel_int/bin/deimos_disasm'
-alias zeus_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$EBM_VERSION/Linux/x86_64/gcc-4.9.2/rel_int/bin/zeus_disasm'
+function _detag_ebm() {
+	export EBM_VERSION=${EBM_VERSION-$($(detag PerformanceModeling:EBM:master::trunk))}
+	echo $EBM_VERSION
+}
+alias ares_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$(_detag_ebm)/Linux/x86_64/gcc-4.9.2/rel_int/bin/ares_disasm'
+alias deimos_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$(_detag_ebm)/Linux/x86_64/gcc-4.9.2/rel_int/bin/deimos_disasm'
+alias zeus_disasm='/arm/devsys-tools/warehouse/PerformanceModeling/EBM/master/$(_detag_ebm)/Linux/x86_64/gcc-4.9.2/rel_int/bin/zeus_disasm'
 
 
 # --- compdef ---
