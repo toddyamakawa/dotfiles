fb3efaa313dd1fe0a562f078c345d778b3649bdb
Todd Yamakawa <todd.yamakawa@arm.com>
2018-06-22 12:08:02 -0500

    zshrc/fzf: Updating widget.

--- zsh/custom/plugins/fzf/fzf.plugin.zsh
+++ zsh/custom/plugins/fzf/fzf.plugin.zsh
@@ -1,4 +1,6 @@
 
+local here=${0:h}
+
 # --- Source ---
 source ~/.fzf.zsh
 local fzfpath=$(git -C $(dirname $(which fzf)) rev-parse --show-toplevel)
@@ -16,21 +18,77 @@ bindkey -M viins '^[[Z' fzf-magic-complete
 bindkey -M viins '^[[Z' fzf-magic-complete
 
 
-# TODO: Write a custom `find` command that searches git files first
+# --- Functions ---
+
+# List Git files
+function fzf-gitfiles() {
+	if [[ -d $GIT_TOP ]]; then
+		git -C $GIT_TOP ls-files | sed -e 's/^/$GIT_TOP\//'
+	fi
+}
+
+# Recrusively list files, shallow first
+function fzf-files() {
+	local dir file here=${1-.}
+	local -U dirs
+	dirs=($here)
+
+	# For each directory
+	while (( ${#dirs} != 0 )); do
+		dir=$dirs[1]
+		dirs[1]=()
+
+		# For each file
+		for file in $(\ls $dir); do
+			entry=$dir/$file
+			[[ -f $entry ]] && echo ${entry#./}
+			[[ -d $entry ]] && dirs+=($entry)
+		done
+	done
+}
+
+# Recrusively list directories, shallow first
+function fzf-dirs() {
+	local dir file here=${1-.}
+	local -U dirs
+	dirs=($here)
+
+	# For each directory
+	while (( ${#dirs} != 0 )); do
+		dir=$dirs[1]
+		dirs[1]=()
+		echo ${dir#./}
+
+		# For each file
+		for file in $(\ls $dir); do
+			entry=$dir/$file
+			[[ -d $entry ]] && dirs+=($entry)
+		done
+	done
+}
+
+function my-fzf-complete() {
+	fzf --height 40% --reverse
+}
+
 
 # --- Widget ---
 # Create fzf-magic-complete widget
 zle -N fzf-magic-complete
 function fzf-magic-complete() {
+	setopt localoptions pipefail 2> /dev/null
+
+	# Directory completion for `cd`
 	if [[ $LBUFFER =~ '^cd\b' ]]; then
-		local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-			-o -type d -print 2> /dev/null | cut -b3-"}"
-		setopt localoptions pipefail 2> /dev/null
-		local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
-		LBUFFER="${LBUFFER}$dir"
+		LBUFFER="${LBUFFER}$(fzf-dirs | my-fzf-complete)"
+
+	# File completion
 	else
-		LBUFFER="${LBUFFER}$(__fsel)"
+		LBUFFER="${LBUFFER}$({fzf-gitfiles; fzf-files} | my-fzf-complete)"
 	fi
+
+	# TODO: Is this stuff needed?
+	# Redraw display
 	local ret=$?
 	zle redisplay
 	typeset -f zle-line-init >/dev/null && zle zle-line-init
