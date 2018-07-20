fb98d49fb12380c5a7cedd4e63031296b73379fb
Todd Yamakawa <todd.yamakawa@arm.com>
2018-07-20 11:47:44 -0500

    vim/template/python: Adding ArgumentParser example.

--- vim/template/header.py
+++ vim/template/header.py
@@ -8,5 +8,36 @@ exec /arm/tools/setup/bin/mrun +python/python/3.6.1 python $0 ${1+"$@"}
 
 import platform
 
-print('python ' + platform.python_version())
+from argparse import ArgumentParser
+from os.path import dirname, abspath
+from time import time
+
+script = abspath(__file__)
+script_dir = dirname(script)
+
+# --- Argument Parser ---
+parser = ArgumentParser()
+parser.add_argument('-v', '--verbose', action='count', help='set verbosity level')
+args = parser.parse_args()
+
+# Get verbosity
+if args.verbose != None:
+	print("running with verbosity " + str(args.verbose))
+	print('python ' + platform.python_version())
+
+# --- Decorator ---
+def decorator(function):
+	def wrapper():
+		print(str(time()) + ' calling ' + str(function))
+		function()
+		print(str(time()) + ' dont ' + str(function))
+	print('Created decorator for ' + str(function))
+	return wrapper
+
+@decorator
+def hi():
+	print('hello world')
+
+hi()
+
 
