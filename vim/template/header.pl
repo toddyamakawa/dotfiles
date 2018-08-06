8a9f78522f3fc40ac95654ad9eecbfc7fb96ae75
Todd Yamakawa <todd.yamakawa@arm.com>
2018-08-06 11:12:55 -0500

    vim/template/perl: Add libraries.

--- vim/template/header.pl
+++ vim/template/header.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 exec /arm/tools/setup/bin/mrun +perl/perl/5.22.1 perl -x -S $0 ${1+"$@"}
 #!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+# Print comand line args
+use Data::Dumper;
+print Dumper \@ARGV;
+
 printf("perl %vd\n", $^V) ;
 
