76f2bff5c021ea929fbe5a531e694135f9d2c9af
Todd Yamakawa <todd.yamakawa@arm.com>
2018-11-09 11:31:07 -0600

    windows/setup.bash: Adding more packages.

--- windows/setup.bash
+++ windows/setup.bash
@@ -2,11 +2,11 @@
 script=$(readlink -f $0)
 here=$(dirname $script)
 
-apt-get install -y zsh git make
+apt-get install -y zsh git vim make curl fontconfig
 git clone https://github.com/toddyamakawa/dotfiles $HOME/dotfiles
 make -C $HOME/dotfiles
 
-apt-get install python-setuptools python-dev build-essential x11-utils
+apt-get install -y python-setuptools python-dev build-essential x11-utils
 easy_install pip
 pip install --upgrade virtualenv
 pip install --user git+git://github.com/Lokaltog/powerline
