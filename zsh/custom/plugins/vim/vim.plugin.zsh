0305e115488db24d2f53ca4b0e60ea1684a34350
Todd Yamakawa <todd.yamakawa@arm.com>
2018-11-28 10:05:52 -0600

    zshrc/vim: Moving suffixes to separate file.

--- zsh/custom/plugins/vim/vim.plugin.zsh
+++ zsh/custom/plugins/vim/vim.plugin.zsh
@@ -1,4 +1,8 @@
 
+local here=${0:h}
+source $here/suffix.zsh
+
+
 # --- Environment Variables ---
 export EDITOR=vim
 export MANPAGER="vim -c MANPAGER -"
@@ -11,40 +15,6 @@ alias vman='vim -c MANPAGER -'
 alias vmin='vim --noplugin'
 alias vnone='vim -u NONE'
 alias vdiff='vimdiff'
-
-# --- Suffix Aliases ---
-
-# Text files
-alias -s err=vim
-alias -s log=vim
-alias -s output=vim
-alias -s ts=vim
-alias -s txt=vim
-
-# Non-executable scripting files
-alias -s pm=vim
-alias -s vim=vim
-
-# Data serialization files
-alias -s cfg=vim
-alias -s conf=vim
-alias -s confg=vim
-alias -s config=vim
-alias -s html=vim
-alias -s json=vim
-alias -s xml=vim
-alias -s yaml=vim
-alias -s yml=vim
-
-# C files
-alias -s c=vim
-alias -s h=vim
-
-# Verilog files
-alias -s v=vim
-alias -s vh=vim
-alias -s sv=vim
-alias -s svh=vim
 
 
 # --- Functions ---
