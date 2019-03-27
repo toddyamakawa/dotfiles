ace70bb65cdb281f2edeabe548ce90af698e9b27
Todd Yamakawa <todd.yamakawa@arm.com>
2019-03-27 13:39:15 -0500

    zshrc/my-zsh: Updating ps aliases and functions.

--- zsh/custom/plugins/my-zsh/alias/ps.zsh
+++ zsh/custom/plugins/my-zsh/alias/ps.zsh
@@ -1,10 +1,17 @@
 
 # --- ps ---
-alias -g pso='-o %cpu,%mem,user,pid,ppid,etime,time,cmd'
-alias psort='ps -e pso | grep -v grep | sort -n'
 alias psef='ps -ef | grep -v grep'
 alias psaux='ps auxw | grep -v grep | sort -n -k3'
 alias psefg='psef | grep'
 alias psauxg='psauxg | grep'
 alias killps='grep -v grep | awk2 | xargs kill'
 
+# --- Functions ---
+function psort() {
+	ps -e -o "%cpu,%mem,user,pid,ppid,etime,time,cmd" | \
+		grep -v grep | sort -n
+}
+
+function pme() {
+	psort | grep $USER | awk '($NF != "-zsh") {$8="\033[1;34m"$8"\033[0m"; print}'
+}
