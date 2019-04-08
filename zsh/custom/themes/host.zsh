27e67b3510dd4e629f308602336e1642174e1874
Todd Yamakawa <todd.yamakawa@arm.com>
2019-04-08 13:57:35 -0500

    zshrc/themes: Removing hostname when using tmux.

--- zsh/custom/themes/host.zsh
+++ zsh/custom/themes/host.zsh
@@ -1,12 +1,12 @@
 
 # --- Host/LSF ---
 # FG White: Default
-# FG Green: tmux is active
+# FG Green: tmux is active, hide hostname
 # FG Yellow: Interactive LSF shell
 function _hostinfo() {
-	local fg=white host='%m'
-	[[ -n $LSB_BATCH_JID ]] && { fg=yellow; host=$LSB_BATCH_JID; }
-	[[ -e ${TMUX%%,*} ]] && fg=green
-	_prompt-fg $fg "%n@$host"
+	local fg=white host='@%m'
+	[[ -n $LSB_BATCH_JID ]] && { fg=yellow; host="@$LSB_BATCH_JID"; }
+	[[ -e ${TMUX%%,*} ]] && { fg=green; host=""; }
+	_prompt-fg $fg "%n$host"
 }
 
