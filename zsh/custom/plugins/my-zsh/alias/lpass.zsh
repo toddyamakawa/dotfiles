f82edc61d8982873f7cdba488c1b439ed4bf80e0
Todd Yamakawa <todd.yamakawa@gmail.com>
2019-04-18 07:05:20 -0500

    zshrc/my-zsh: Adding function to get lastpass info.

--- zsh/custom/plugins/my-zsh/alias/lpass.zsh
+++ zsh/custom/plugins/my-zsh/alias/lpass.zsh
@@ -3,8 +3,29 @@ alias lp='lpass'
 alias lpstat='lpass status'
 alias lplogin='lpass login'
 
-function lpfzf() {
+# --- Search for LastPass Info ---
+function lpf() {
 	lpass status || return 1
-	lpass ls --long | fzf
+	local ret selected id
+
+	# Select site with fzf
+	selected=($(
+		lp ls --long | \
+			awk '/username: \w/{$1="";$2="";print}' | \
+			sed -e 's/\(.*\)\[id:\s*\(\w\+\).*username:\s*\(.*\)\]/\2,\3,\1/' | \
+			column -t -s ',' | \
+			fzf
+	))
+	ret=$?
+
+	# Print site information and copy password
+	if [[ $ret -eq 0 ]]; then
+		id=$selected[1]
+		lp show --url $id
+		echo "Username: $selected[2]"
+		lp show --password $id | xclip
+		#lp show --clip --password $id
+		echo "Password copied to clipboard"
+	fi
 }
 
