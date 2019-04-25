2f09a2f6a86c36a6c88ef10f623a7a909d7d2f3a
Todd Yamakawa <todd.yamakawa@arm.com>
2019-04-25 09:27:21 -0500

    zshrc/regex: Deleting unused grep aliases.

--- zsh/custom/plugins/regex/grep.zsh
+++ zsh/custom/plugins/regex/grep.zsh
@@ -2,14 +2,6 @@
 # oh-my-zsh already aliases grep
 # grep: aliased to grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}
 
-alias g='grep'
-alias gi='grep -i'
-alias gn='grep -n'
-alias gr='grep -r'
-alias gv='grep -v'
-alias gin='grep -in'
-alias grn='grep -rn'
-alias grin='grep -rin'
 alias gu='grep $(whoami)'
 alias psg='psaux | g -i -e VSZ -e'
 
