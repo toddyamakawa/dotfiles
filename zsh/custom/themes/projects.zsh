521e9c03ac94341137df97050ab66e956957d833
Todd Yamakawa <todd.yamakawa@arm.com>
2019-04-29 14:39:25 -0500

    zshrc/themes: Fixing project status checker.

--- zsh/custom/themes/projects.zsh
+++ zsh/custom/themes/projects.zsh
@@ -5,9 +5,10 @@
 
 function _projects-status() {
 	[[ -d $PROJ_HOME ]] || return
-	local star=$_ZSH_STAR_HOLLOW fg=red
+	local star=$_ZSH_STAR_HOLLOW fg=red simcmd
 	[[ -d $WORK_DIR ]] && star=$_ZSH_STAR_SOLID
-	which sim_tb &>/dev/null && fg=green
+	simcmd=$(\which sim_tb 2>/dev/null)
+	[[ $simcmd -ef $PROJ_HOME/tools/shared/bin/sim_tb ]] && fg=green
 	_prompt-fg $fg $star
 }
 
